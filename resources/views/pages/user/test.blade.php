c73391c5248100064e386fa85d86cd51f564edd7
easti <eastidmyt@gmail.com>
2024-04-01 13:00:35 +0700

    hmmm

--- resources/views/pages/user/test.blade.php
+++ resources/views/pages/user/test.blade.php
@@ -2,24 +2,31 @@
 
 @section('content')
     <!-- The sidebar -->
-    <link href="{{ asset('style.css') }}" rel="stylesheet" type="text/css">
-    <nav class="navbar bg-body-tertiary fw-smaller">
-        <div class="container">
-            <button class="btn btn-outline-secondary btn-sm"><a class="navbar-brand bi bi-houses"
-                    href="#">Home</a></button>
-            <button class="btn btn-outline-secondary btn-sm"><a class="navbar-brand bi bi-pencil-square" href="#">Start
-                    Test</a></button>
-            <button class="btn btn-outline-secondary btn-sm"><a class="navbar-brand bi bi-clock-history"
-                    href="#">History Test</a></button>
+    <div class="row">
+        <div class="col-12">
+            <link href="{{ asset('style.css') }}" rel="stylesheet" type="text/css">
+            <nav class="navbar bg-body-tertiary fw-smaller">
+                <div class="container">
+                    <button class="btn btn-outline-secondary btn-sm"><a class="navbar-brand bi bi-houses"
+                            href="#">Home</a></button>
+                    <button class="btn btn-outline-secondary btn-sm"><a class="navbar-brand bi bi-pencil-square"
+                            href="#">Start
+                            Test</a></button>
+                    <button class="btn btn-outline-secondary btn-sm"><a class="navbar-brand bi bi-clock-history"
+                            href="#">History Test</a></button>
+                </div>
+            </nav>
         </div>
-    </nav>
-    <div class="container">
-        <div class="row">
-          
-            <div class="col-8"></div>
-            <h4>Welcome to the POLINDRA TOEFL Prediction Test</h4>
-                <p class="text center">This test simulation was design for you whose ready to improve
-                    through this simulation test, with the simmiliar huhu
-                </p>
     </div>
-@endsection
+    <div class="row">
+        <div class="col-4">
+            <img src="../img/dashboard.png" alt="" class="responsive">
+        </div>
+
+        <div class="col-8">
+            <h4>Welcome to the POLINDRA TOEFL Prediction Test</h4>
+            <p class="text center">This test simulation was design for you whose ready to improve
+                through this simulation test, ..... bingung
+            </p>
+        </div>
+    @endsection
