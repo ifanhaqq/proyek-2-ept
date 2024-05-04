90129aefc3fe430c819c1d3db7c9fbc8469a83a4
enjoyuri <haqinhanifan@gmail.com>
2024-05-04 23:58:26 +0700

    add question crud finally over

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -109,9 +109,13 @@ class AdminController extends Controller
     {
         // dd($request);
         $testQuestion = new TestQuestion;
+
         $audio_id = TestWave::where('wave_id', $request->wave_id)->value('audio_id');
 
+
+
         if ($request->addQuestionSection == 'listening') {
+
 
             $testQuestion->wave_id = $request->wave_id;
             $testQuestion->audio_id = $audio_id;
@@ -146,10 +150,17 @@ class AdminController extends Controller
 
         } else if ($request->addQuestionSection == 'reading') {
 
+            $readingSection = new ReadingSection;
+            $readingId = count(ReadingSection::get()) + 1;
 
-            dd($request);
+            $readingSection->reading_id = $readingId;
+            $readingSection->text = $request->readingTextQuestion;
+
+            $readingSection->save();
+            // dd($request);
 
             $testQuestion->wave_id = $request->wave_id;
+            $testQuestion->reading_id = $readingId;
 
             $testQuestion->section = $request->addQuestionSection;
             $testQuestion->question = $request->question;
@@ -164,18 +175,23 @@ class AdminController extends Controller
             $testQuestion->save();
 
             if ($request->questionElemParam > 1) {
+                // dd($request);
                 for ($i = 2; $i <= $request->questionElemParam; $i++) {
+                    $testQuestion = new TestQuestion;
+
+                    $testQuestion->wave_id = $request->wave_id;
+                    $testQuestion->reading_id = $readingId;
+
+                    $testQuestion->section = $request->addQuestionSection;
                     $testQuestion->question = $request->input("question_$i");
 
                     $correctAnswer = $request->input("correct_answer_$i");
 
-                    echo $correctAnswer;
-
-                    $testQuestion->question_ch1 = $request->input("choice_1_$i");
+                    $testQuestion->question_ch1 = $request->input("choice_1_" . $i);
                     $testQuestion->question_ch2 = $request->input("choice_2_$i");
                     $testQuestion->question_ch3 = $request->input("choice_3_$i");
                     $testQuestion->question_ch4 = $request->input("choice_4_$i");
-                    $testQuestion->correct_answer = $request->input("choice_" . $correctAnswer  . "_$i");
+                    $testQuestion->correct_answer = $request->input("choice_" . $correctAnswer . "_$i");
 
                     $testQuestion->save();
                 }
