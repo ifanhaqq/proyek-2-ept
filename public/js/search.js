04e3172a73258a39e728d57558958974b126c0f5
enjoyuri <haqinhanifan@gmail.com>
2024-06-20 10:31:09 +0700

    search feature on test taker results

--- public/js/search.js
+++ public/js/search.js
@@ -1,16 +1,27 @@
 $(document).ready(function () {
-    let input, filter, table, tr, td, i, txtvalues, search
+    // let input, filter, table, tr, td, i, txtvalues, search
 
-    input = $("#searchInput")
-    filter = input.val(function () {
-        return this.value.toUpperCase()
-    })
-    table = $("#scoresTable")
-    tr = $("tr")
-    td = $("td")
+    // input = $("#searchInput")
+    // filter = input.val(function () {
+    //     return this.value.toUpperCase()
+    // })
+    // table = $("#scoresTable")
+    // tr = $("tr")
+    // td = $("td")
 
-    for (let index = 0; index < tr.length; index++) {
-        search = tr[i].td[0]
+    // for (let index = 0; index < tr.length; index++) {
+    //     search = tr[i].td[0]
         
-    }
+    // }
+
+    let input = $('#searchInput')
+    let table = $('#scoresTable')
+
+    $(input).keyup(function (e) { 
+        let value = $(this).val().toLowerCase()
+        $('#scoresTable tbody tr').filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        })
+        console.log(value)
+    });
 })
